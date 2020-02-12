dc42b6baad28af38c9be5a05b1f29f928d2a010f
LudiusMaximus <4678285+LudiusMaximus@users.noreply.github.com>
2020-02-12 03:04:24 +0100

    Further adjustments.

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local eventFrame = CreateFrame("Frame")
 
 eventFrame:RegisterEvent("GOSSIP_SHOW")
 eventFrame:RegisterEvent("QUEST_GREETING")
-eventFrame:RegisterEvent("QUESTLINE_UPDATE")
+eventFrame:RegisterEvent("QUEST_FINISHED")
 eventFrame:RegisterEvent("GOSSIP_CLOSED")
 eventFrame:RegisterEvent("QUEST_DETAIL")
 eventFrame:RegisterEvent("QUEST_ACCEPTED")
@@ -20,34 +20,58 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 
   -- print(event, questDetailsOpened)
 
-  if event == "GOSSIP_SHOW" or event == "QUEST_GREETING" or event == "QUESTLINE_UPDATE" then
+  -- Initialise NPC interaction.
+  -- Some NPC interaction is initialised with just QUESTLINE_UPDATE,
+  -- but we leave this out here for now!
+  if event == "GOSSIP_SHOW" or event == "QUEST_GREETING" then
     questDetailsOpened = 0
     gossipShown = true
     L:CancelAllTimers()
-  end
 
-  if event == "GOSSIP_CLOSED" then
+
+  -- To reset the questDetailsOpened counter,
+  -- if a quest is started with QUEST_DETAIL is declined.
+  elseif event == "QUEST_FINISHED" then
+    if not QuestFrame:IsShown() then
+      questDetailsOpened = 0
+      gossipShown = false
+    end
+    
+    
+    
+  elseif event == "GOSSIP_CLOSED" then
     gossipShown = false
     
+
+  -- The order in which QUEST_ACCEPTED and QUEST_DETAIL of the next quest happen is indeterministic.
+  -- Hence we have to use this counter such that CloseIfDone() finds 1 or 0 depending
+  -- on whether another QUEST_DETAIL has been opened.
   elseif event == "QUEST_DETAIL" then
     questDetailsOpened = questDetailsOpened + 1
+    -- Must not set gossipShown = true here, otherwise it will not close after QUEST_ACCEPTED.
     
-  elseif event == "QUEST_TURNED_IN" or (event == "QUEST_ACCEPTED" and not QuestGetAutoAccept()) then
+  elseif event == "QUEST_ACCEPTED" and not QuestGetAutoAccept() then
     questDetailsOpened = questDetailsOpened - 1
-    -- Must not be negative!
+    -- Must not be negative, otherwise a later QUEST_DETAIL may not increase over 0.
     if questDetailsOpened < 0 then
       questDetailsOpened = 0
     end
-    L:ScheduleTimer("closeIfDone", 0.3)
+    L:ScheduleTimer("CloseIfDone", 0.3)
+  
+  
+  -- Some quests (e.g. "Wolves at Our Heels") also do not close after handing them in!
+  elseif event == "QUEST_TURNED_IN" then
+    L:ScheduleTimer("CloseIfDone", 0.3)
+  
   end
 
-  -- print("questDetailsOpened", questDetailsOpened)
+  -- print("questDetailsOpened", questDetailsOpened, gossipShown)
 
 end)
 
 
 
-function L:closeIfDone()
+function L:CloseIfDone()
 
   if questDetailsOpened < 1 and not gossipShown then
     -- print("Closing Quest")
